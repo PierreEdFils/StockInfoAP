2aa74e4eb0ed713b22c0f9b1229859e90abb384a
PierreEdFils <epierrefils@gmail.com>
2022-03-07 18:04:49 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/StockInfoAP/StockInfoApApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.StockInfoAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInfoApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/StockInfoAP/StockInfoApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.StockInfoAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInfoApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
